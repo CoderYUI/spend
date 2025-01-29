04e375912100b82b695a7877a95d6dc09dc9a5bb
CoderYUI <bhaskar.23bey10012@vitbhopal.ac.in>
2025-01-29 23:11:14 +0530

    fx69

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,25 @@ export default defineConfig({
   base: './',
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
     rollupOptions: {
       input: {
         index: resolve(__dirname, 'index.html'),
         dashboard: resolve(__dirname, 'dashboard.html'),
         about: resolve(__dirname, 'about.html'),
         reward: resolve(__dirname, 'reward.html')
+      },
+      output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
       }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
     }
   }
 })
