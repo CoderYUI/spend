b3de76b8fa00c29a8e2b6a3fb1a1a7164124cc77
CoderYUI <bhaskar.23bey10012@vitbhopal.ac.in>
2025-01-29 23:13:33 +0530

    dsadasdad

--- vite.config.js
+++ vite.config.js
@@ -2,28 +2,20 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
+  root: process.cwd(),
   base: './',
+  publicDir: 'public',
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+    target: 'es2015',
     rollupOptions: {
       input: {
-        index: resolve(__dirname, 'index.html'),
+        main: resolve(__dirname, 'index.html'),
         dashboard: resolve(__dirname, 'dashboard.html'),
         about: resolve(__dirname, 'about.html'),
         reward: resolve(__dirname, 'reward.html')
-      },
-      output: {
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
       }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
     }
   }
 })
