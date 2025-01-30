a51d5dd4a2c252c64e03dca1f3704c25f35b0a50
CoderYUI <bhaskar.23bey10012@vitbhopal.ac.in>
2025-01-30 12:25:23 +0530

    hjjjbjbjbb

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: '.',
   base: '/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: '/index.html',
         dashboard: '/dashboard/index.html'
       }
     }
+  },
+  server: {
+    port: 3000
   }
 });
