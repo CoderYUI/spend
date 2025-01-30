91858af98ac064c09e076ece409d43c0c13fe97c
CoderYUI <bhaskar.23bey10012@vitbhopal.ac.in>
2025-01-30 14:42:54 +0530

    asda

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'public',
   build: {
-    outDir: '../dist', // Output directory for the build
     rollupOptions: {
       input: {
         main: 'public/index.html',
         dashboard: 'public/dashboard.html',
-      }
-    }
+      },
+    },
   },
 });
